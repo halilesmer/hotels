a771392bb76d59a8c2326da04fb8018e1b28b86f
halilesmer <halil@esmer.de>
2022-07-29 07:38:59 +0200

    show favorit cards in myCard page

--- src/views/MyCards.js
+++ src/views/MyCards.js
@@ -1,30 +1,74 @@
 import { Box, Container } from "@mui/system";
+import { Button, CircularProgress, Grid } from "@mui/material";
 import React, { useContext, useState } from "react";
 
 import { AppContext } from "../component/context/appContext.js";
-import { Button } from "@mui/material";
+import OneCard from "../component/OneCard.js";
 
 const MyCards = () => {
 
-  const [numb, setNumb] = useState([]);
-
-  // const cardsId = [];
  
-  const {cardsId, setCardsId}= useContext(AppContext);
+  const { isLoading, data, isError, cardsId, setCardsId, handleDeleteCardClick, handleAddCardClick,  } =
+    useContext(AppContext);
 
   
-      console.log("numb: ", numb);
   console.log("cardsId: ", cardsId);
   return (
-    <Container>
-      <Box>
-        <Button>Add Card Id</Button>
-        { cardsId.length > 0 && cardsId.map((card, i) => <li key={i}>{card}</li>)}
 
-        <Button onClick={() => setCardsId("")}>Delete All Cards</Button>
+      <Box> 
+             <Button onClick={() => setCardsId("")}>Delete All Cards</Button>
+
+      {isLoading && <CircularProgress color="inherit" />}
+      <Grid container spacing={1}>
+        {data &&
+          data.map((card) => {
+            return (
+              cardsId.map(crdId =>{
+
+return (
+  crdId === card.id && (
+    <Grid
+      key={card.id}
+      item
+      xs={12}
+      sm={12}
+      md={6}
+      lg={4}
+      xl={3}
+
+      // style={{ margin: "0 auto 4rem auto" }}
+    >
+      {<OneCard key={card.id} card={card} />}
+    </Grid>
+  )
+);
+              })
+
+
+            
+            );
+          })}
+      </Grid>
+
       </Box>
-    </Container>
+
   );
 };
 
 export default MyCards;
+
+
+/*  <Grid
+              key={card.id}
+                item
+                xs={12}
+                sm={12}
+                md={6}
+                lg={4}
+                xl={3}
+
+              // style={{ margin: "0 auto 4rem auto" }}
+              >
+                {  <OneCard key={card.id} card={card} />}
+              </Grid>
+ */
