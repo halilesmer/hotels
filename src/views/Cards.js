4f9f360a5960cade402f9e94b1719fd61cab381b
halilesmer <halil@esmer.de>
2022-07-31 19:29:42 +0200

    fixed appearing paginatin after searching

--- src/views/Cards.js
+++ src/views/Cards.js
@@ -30,28 +30,30 @@ function Cards() {
           data.map((card) => {
             return (
               card.imageUrl && (
-                <Grid
-                  key={card.id}
-                  item
-                  xs={12}
-                  sm={12}
-                  md={6}
-                  lg={4}
-                  xl={4}
-                  style={{ paddingTop: "0" }}
+                <>
+                  <Grid
+                    key={card.id}
+                    item
+                    xs={12}
+                    sm={12}
+                    md={6}
+                    lg={4}
+                    xl={4}
+                    style={{ paddingTop: "0" }}
 
-                  // style={{ margin: "0 auto 4rem auto" }}
-                >
-                  <OneCard cardId={card.id} card={card} />
-                </Grid>
+                    // style={{ margin: "0 auto 4rem auto" }}
+                  >
+                    <OneCard cardId={card.id} card={card} />
+                  </Grid>
+                </>
               )
             );
           })}
       </Grid>
+      {data && data.length>0 && <PaginationCon data={data} />}
 
-      <PaginationCon data={data} />
       {data && data.length < 1 && (
-        <Typography mt={12} align='center' className="searchResultAlert">
+        <Typography mt={12} align="center" className="searchResultAlert">
           "I'm sorry. I was not able to find a match."
         </Typography>
       )}
