5642035abb06f5bced6f5bea45098755951a5739
halilesmer <halil@esmer.de>
2022-08-01 22:49:40 +0200

    configed config file

--- src/App.js
+++ src/App.js
@@ -6,8 +6,10 @@ import { Container } from "@mui/system";
 import FooterBanner from "./component/FooterBanner";
 import Main from "./views/Main";
 import NavBanner from "./views/NavBanner";
+import { app } from "./config/config";
 
 function App() {
+  console.log('app', app)
   return (
     <>
       <NavBanner />
