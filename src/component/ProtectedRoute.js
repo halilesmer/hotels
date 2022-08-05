1a156b66aefda6f1423bb28a530486b68ac4e8b0
halilesmer <halil@esmer.de>
2022-08-05 16:50:34 +0200

    change in protected route to check netlify error

--- src/component/ProtectedRoute.js
+++ src/component/ProtectedRoute.js
@@ -10,18 +10,22 @@ const ProtectedRoute = ({children}) => {
     const {loading, user} = useContext(AuthContext);
     // const isAuth = useIsAuthenticated();
    
- 
+ console.log('loading, user :>> ', loading, user);
 
 
   return (
-    <div className="protected-route">
-      {loading ? (
-       <Loading/>
-      ) : user ? (
-        children
-      ) : (
-        <Navigate to="/" replace={true} />
-      )}
+    // <div className="protected-route">
+    //   {loading ? (
+    //    <Loading/>
+       
+    //   ) : user ? (
+    //     children
+    //   ) : (
+    //     <Navigate to="/" replace={true} />
+    //   )}
+    // </div>
+    <div>
+      {loading ? <p>loading</p> : user ? children : <Navigate to="/"/>}
     </div>
   );
 }
